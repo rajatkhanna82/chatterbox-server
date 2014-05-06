4f258763dc00d43cab84259fe8fd6989c44d0456
Hack Reactor Students <rajatkhanna82@gmail.com>
2014-05-05 20:35:19 -0700

    Added functionality for POST,GET and OPTIONS

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,14 +5,22 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
+var results = [];
+var msg = {};
+
+
+
+exports.handler = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
+  // console.log("Serving request type " + request.method + " for url " + request.url);
   console.log("Serving request type " + request.method + " for url " + request.url);
+
+  //message storage
 
   var statusCode = 200;
 
@@ -21,15 +29,70 @@ var handleRequest = function(request, response) {
   var headers = defaultCorsHeaders;
 
   headers['Content-Type'] = "text/plain";
+  var getUID = function(){
+    return 'xxxxxx'.replace(/[x]/g, function(){
+      var r = Math.random()*16|0;
+      return r.toString(16);
+    });
+  };
+  var writeMessage = function(request, response, callback){
+    response.writeHead(200, headers);
+    response.end(JSON.stringify("OK"));
+  };
 
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+  var saveMessage = function(request, response, callback){
+    request.on("data", function(data){
+      msg = JSON.parse(data.toString());
+      msg.createdAt = new Date();
+      msg.objectId = getUID();
+      results.push(msg);
+      console.log(results);
+      response.writeHead(200, headers);
+      response.end(JSON.stringify(msg));
+    });
+  };
 
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!");
+  var readMessage = function(request, response, callback){
+    response.writeHead(200, headers);
+    response.end(JSON.stringify({'results':results}));
+  };
+
+  var requestHandler = {
+    'OPTIONS' : writeMessage,
+    'POST': saveMessage,
+    'GET': readMessage,
+  };
+
+  requestHandler[request.method](request,response);
+
+  // if (request.method === 'OPTIONS'){
+  //   statusCode = 200;
+  // }else if(request.method === 'POST'){
+  //   saveMessage();
+
+  //   // console.log("POST",request);
+  //   // request.on("data", function(data){
+  //   //   msg = JSON.parse(data.toString());
+  //   //   msg.createdAt = new Date();
+  //   //   results.push(msg);
+  //   //   console.log(results);
+  //   // });
+  //   // storage.push(data);
+
+
+  // }else if(request.method === 'GET'){
+  //   // console.log("GET",request);
+  //   writeMessage();
+  // }
+
+  // /* .writeHead() tells our server what HTTP status code to send back */
+  // response.writeHead(statusCode, headers);
+
+  // /* Make sure to always call response.end() - Node will not send
+  //  * anything back to the client until you do. The string you pass to
+  //  * response.end() will be the body of the response - i.e. what shows
+  //  * up in the browser.*/
+  // response.end("Hello, World!");
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
